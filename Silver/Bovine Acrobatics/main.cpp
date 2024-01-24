03459165526006ebc36bd14a598fca746d79b4f1
Aster <aster.deng@gmail.com>
2024-01-23 21:54:05 -0800

    Add Bovine Acrobatics

---
+++ Silver/Bovine Acrobatics/main.cpp
@@ -0,0 +1,11 @@
+// http://www.usaco.org/index.php?page=viewproblem2&cpid=1350
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+	int N, M, K; cin >> N >> M >> K;
+}
