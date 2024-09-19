006e8024ea2fdf225bbcf7bc08edd41d6f98dd1b
Aster Deng <46637392+videogamesrockp@users.noreply.github.com>
2024-09-18 21:05:40 -0700

    Add files via upload

---
+++ Silver/grass-planting.cpp
@@ -0,0 +1,27 @@
+// Source: https://usaco.guide/general/io
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	freopen("planting.in", "r", stdin);
+	freopen("planting.out", "w", stdout);
+	int N; cin >> N;
+
+	vector<int> neighbors (N);
+
+	for (int i = 0; i < N - 1; i ++) {
+		int a, b; cin >> a >> b;
+		neighbors[a-1]++;
+		neighbors[b-1]++;
+	}
+
+	int ans = 0;
+	for (int i: neighbors) {
+		if (i > ans) {
+			ans = i;
+		}
+	}
+
+	cout << ans + 1;
+}
